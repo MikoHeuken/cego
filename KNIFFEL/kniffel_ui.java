5e0157772166b259d8007b44fd93c2e38b410493
Miko <mheuken@stud.hs-offenburg.de>
2024-04-25 00:28:45 +0200

    added simple logic to play rounds

--- KNIFFEL/kniffel_ui.java
+++ KNIFFEL/kniffel_ui.java
@@ -1,6 +1,7 @@
 package KNIFFEL;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,11 +26,15 @@ public class kniffel_ui extends JFrame implements ActionListener {
     public kniffel_ui(kniffel_player[] player) {
         this.player = player;
         numberOfPlayers = player.length;
+
         setTitle("Kniffel");
-        setSize(4000, 3000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         JPanel mainPanel = new JPanel(new BorderLayout());
+
+        int borderThickness = (int) (Toolkit.getDefaultToolkit().getScreenResolution() * 0.2);
+        mainPanel.setBorder(new EmptyBorder(borderThickness, borderThickness, borderThickness, borderThickness));
 
         //Kategorien-Panel, wird an der linken Seite angezeigt
         JPanel categoriesPanel = new JPanel(new GridLayout(categories.length + 1, 1));
@@ -40,15 +45,16 @@ public class kniffel_ui extends JFrame implements ActionListener {
 
         //Spieler-Panel, wird oben horizontal angezeigt
         JPanel playerPanel = new JPanel(new GridLayout(1, numberOfPlayers + 1));
-        scoreSelectors = new JComboBox[categories.length][numberOfPlayers];
+        scoreSelectors = new JComboBox[numberOfPlayers][categories.length];
 
         //fügt jedem Spieler für jede Kategorie ein passendes Dropdown Menu zum Auswählen der Punkte hinzu
         for (int i = 0; i < numberOfPlayers; i++) {
             JPanel playerColumn = new JPanel(new GridLayout(categories.length + 1, 1));
                 playerColumn.add(new JLabel(player[i].getName()));
             for (int j = 0; j < categories.length; j++) {
-                scoreSelectors[j][i] = new JComboBox<>(dropDownOptions(categories[j]));
-                playerColumn.add(scoreSelectors[j][i]);
+                scoreSelectors[i][j] = new JComboBox<>(dropDownOptions(categories[j]));
+                scoreSelectors[i][j].setSelectedItem(null);
+                playerColumn.add(scoreSelectors[i][j]);
             }
             playerPanel.add(playerColumn);
         }
@@ -75,7 +81,7 @@ public class kniffel_ui extends JFrame implements ActionListener {
      */
     private Integer[] dropDownOptions(String categorie){
       ArrayList<Integer> options = new ArrayList<>();
-      options.add(0);
+      options.add(null);
       
       switch (categorie) {
         case "1er":
@@ -150,8 +156,81 @@ public class kniffel_ui extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == nextPlayerButton) {
-          
+          int chosenCategorie = whichOptionWasChosen(currentPlayerIndex);
+          if(chosenCategorie != -1 && onlyOneOptionChosen(currentPlayerIndex)){
+            if(onlyCurrentPlayerChose(currentPlayerIndex)){
+              logCategorie(chosenCategorie, currentPlayerIndex);
+              nextPlayer();
+            }else{
+              JOptionPane.showMessageDialog(this, "In dieser Runde darf nur " + player[currentPlayerIndex].getName() + " eine Auswahl treffen.", "Fehler", JOptionPane.ERROR_MESSAGE);
+            }
+          }else{
+            JOptionPane.showMessageDialog(this, "Es darf nur genau eine Option ausgewählt sein.", "Fehler", JOptionPane.ERROR_MESSAGE);
+          }
         }
+    }
+
+    /**
+     * setzt currentPlayerIndex auf den nächsten Spieler
+     */
+    public void nextPlayer(){
+      if(currentPlayerIndex == numberOfPlayers - 1){
+        currentPlayerIndex = 0;
+      }else{
+        currentPlayerIndex++;
+      }
+    }
+
+    public boolean onlyCurrentPlayerChose(int playerNr){
+      for(int i = 0; i < numberOfPlayers; i++){
+        for(int j = 0; j < scoreSelectors[i].length; j++){
+          if(i != currentPlayerIndex){
+            if(scoreSelectors[i][j].getSelectedItem() != null && scoreSelectors[i][j].isEnabled()){
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    }
+
+    /**
+     * loggt die Ausgeählte Kategorie für den Spieler ein und gibt dem Spieler die entsprechenden Punkte
+     * @param categorieIndex der Index der eingeloggten Kategorie
+     * @param currentPlayerIndex der betroffene Spieler
+     */
+    public void logCategorie(int categorieIndex, int currentPlayerIndex){
+      int chosenOption = (Integer) scoreSelectors[currentPlayerIndex][categorieIndex].getSelectedItem();
+      scoreSelectors[currentPlayerIndex][categorieIndex].setEnabled(false);
+      player[currentPlayerIndex].changePoints(chosenOption);
+    }
+
+    public boolean onlyOneOptionChosen(int playerNr){
+      boolean onlyOptionChosen = false;
+      for(int i = 0; i < scoreSelectors[playerNr].length; i++){
+        if(scoreSelectors[playerNr][i].getSelectedItem() != null && scoreSelectors[playerNr][i].isEnabled()){
+          if(onlyOptionChosen){
+            return false;
+          }else{
+            onlyOptionChosen = true;
+          }
+        }
+      }
+      return onlyOptionChosen;
+    }
+
+    /**
+     * gibt aus, welche Option der Spieler ausgewähl hat
+     * @param playerNr der Spieler, der an der Reihe ist
+     * @return den Index der ausgewählten Option, -1 wenn keine Opion ausgewählt wurde
+     */
+    public int whichOptionWasChosen(int playerNr){
+      for(int i = 0; i < scoreSelectors[playerNr].length; i++){
+          if(scoreSelectors[playerNr][i].getSelectedItem() != null && scoreSelectors[playerNr][i].isEnabled()){
+            return i;
+          }
+      }
+      return -1;
     }
 
     public static void main(String[] args) {
